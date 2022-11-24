8e0334a4b71ac51a7196ab88973213453e352c43
paduck-96 <peterrabitt1234@naver.com>
2022-11-24 11:59:57 +0900

    feat: node project modules settings .env , server setting, log creating, log setting, body-parser, db connection, db setting,  session-store, file-upload, static file, file-download, err handling

--- NODE/nodeConnection/app.js
+++ NODE/nodeConnection/app.js
@@ -1,37 +1,107 @@
-const mariadb = require("mysql");
+const express = require("express");
+const morgan = require("morgan");
+const compression = require("compression");
+const path = require("path");
+const mysql = require("mysql");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const multer = require("multer");
 const dotenv = require("dotenv");
 
+//.env 사용
 dotenv.config();
 
-//db 정보 전달
-let connection = mariadb.createConnection({
-  host: "127.0.0.1",
-  port: 3306,
-  user: "root",
+// 서버 설정
+const app = express();
+app.set("port", process.env.PORT || 9000);
+
+// 로그 기록
+const fileStreamRotator = require("file-stream-rotator");
+const fs = require("fs");
+let logDirectory = path.join(__dirname, "/log");
+// 폴더 없으면 생성
+fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
+// 로그 파일 옵션
+let accessLogStream = fileStreamRotator.getStream({
+  date_format: "YYYYMMDD",
+  filename: path.join(logDirectory, "access-%DATE%.log"),
+  frequency: "daily",
+  verbose: false,
+});
+// 로그 기록 설정
+app.use(morgan("combined", { stream: accessLogStream }));
+
+// 서버 압축해서 클라이언트로 전송
+app.use(compression());
+
+// post 파라미터 읽기
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    //post 요청에 대해 application server 가  인코딩
+    extended: true,
+  })
+);
+
+// 데이터베이스 접속 정보
+let options = {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
-  database: "nodeConnection",
+  database: process.env.DB_NAME,
+};
+
+//세션 저장 MySQL 데이터베이스 생성
+const mariaDBStore = require("express-mysql-session")(session);
+//세션설정
+app.use(
+  session({
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    store: new mariaDBStore(options),
+  })
+);
+
+//파일 업로드 설정
+const upload = multer({
+  storage: multer.diskStorage({
+    destination(req, file, done) {
+      done(null, "public/img");
+    },
+    filename(req, file, done) {
+      const ext = path.extname(file.originalname);
+      done(null, path.basename(file.originalname, ext) + Date.now() + ext);
+    },
+  }),
+  limits: { fileSize: 10 * 1024 * 1024 },
 });
 
-//연결 확인
+// 정적 파일 경로 설정
+app.use("/", express.static("public"));
+
+// 파일 다운로드 모듈
+let util = require("util");
+let mime = require("mime");
+
+// 데이터베이스 연결
+let connection = mysql.createConnection(options);
 connection.connect((err) => {
   if (err) {
-    throw new Error("DB Connection ❌ :" + err);
+    console.error(err);
   } else {
-    console.log("DB Connection ✔");
-    // sql구문 문자열로 전달
-    // connection.query(
-    //   "create table family(id int auto_increment primary key, name varchar(20))"
-    // );
-    // connection.query("insert into family(name) values(?)", "박문석");
-    // connection.query("insert into family(name) values(?)", "예시");
-    // select
-    connection.query("select * from family", (err, results, fields) => {
-      if (err) {
-        throw new Error("{result:false, message: }", err.code);
-      } else {
-        let result = JSON.stringify(results);
-        console.log(result);
-      }
-    });
+    //
   }
+});
+
+// 에러 핸들링
+app.use((err, req, res, next) => {
+  res.status(500).send(err.message);
+});
+
+app.listen(app.get("port"), () => {
+  console.log(`Listening to PORT...${app.get("port")}
+  explorer.exe http://localhost:${app.get("port")}`);
 });
