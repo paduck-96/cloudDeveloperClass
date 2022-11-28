5e7e693b1d6a48077d0d1570bf5aa32adde945f0
paduck-96 <peterrabitt1234@naver.com>
2022-11-28 17:47:45 +0900

    feat: User, Comment로 삽입

--- NODE/dbConnection/app.js
+++ NODE/dbConnection/app.js
@@ -6,6 +6,9 @@ const { sequelize } = require("./models");
 const app = express();
 app.set("port", process.env.DB_PORT || 9000);
 
+const User = require("./models/user");
+const Comment = require("./models/comment");
+
 // sequelize 연결
 sequelize
   .sync({ force: false })
@@ -15,6 +18,14 @@ sequelize
   .catch((err) => {
     console.log("데이터 연결 ❌..." + err);
   });
+
+//사용
+app.get("/", async (req, res) => {
+  User.create({
+    name: "(☞ﾟヮﾟ)☞",
+    age: 25,
+  });
+});
 
 app.listen(app.get("port"), () => {
   console.log("http://localhost:" + app.get("port"));
