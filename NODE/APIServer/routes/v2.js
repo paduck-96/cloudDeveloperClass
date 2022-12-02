f37c29eea70a39555ad08e4f71e0edd6b5f4c050
paduck-96 <peterrabitt1234@naver.com>
2022-12-02 12:30:31 +0900

    feat: 사용량 제한 적용된 토큰 요청 파일

---
+++ NODE/APIServer/routes/v2.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const { verifyToken, apiLimiter } = require("./middlewares");
+const jwt = require("jsonwebtoken");
+const { Domain, Hashtag, Post, User } = require("../models");
+
+const router = express.Router();
+
+// 데이터 리턴 요청 처리
+router.get("/posts/my", apiLimiter, verifyToken, (req, res) => {
+  Post.findAll({ where: { userId: req.decoded.id } })
+    .then((posts) => {
+      console.log(posts);
+      res.json({ code: 200, payload: posts });
+    })
+    .catch((err) => {
+      console.error(err);
+      return res.status(500).json({ code: 500, message: "서버 에러" });
+    });
+});
+// 토큰 확인 처리
+router.post("/token", apiLimiter, async (req, res) => {
+  const { clientSecret } = req.body;
+  try {
+    // 도메인 찾아오기
+    const domain = await Domain.findOne({
+      where: { clientSecret },
+      include: { model: User, attribute: ["nick", "id"] },
+    });
+    if (!domain) {
+      return res.status(401).json({
+        code: 401,
+        message: "미등록 도메인",
+      });
+    }
+    const token = jwt.sign(
+      {
+        id: domain.User.id,
+        nick: domain.User.nick,
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: "1m", issuer: "adam" }
+    );
+
+    return res.json({
+      code: 200,
+      message: "토큰 발급 완료",
+      token,
+    });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({
+      code: 500,
+      message: "서버에러",
+    });
+  }
+});
+
+// 테스트 시 동작
+// 토큰을 확인하기 위한 처리
+router.get("/test", apiLimiter, verifyToken, (req, res) => {
+  res.json(req.decoded);
+});
+
+module.exports = router;
