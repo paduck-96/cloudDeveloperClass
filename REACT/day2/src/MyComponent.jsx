1b329f668344011f8e656112ae42da2e5e8d4801
paduck-96 <peterrabitt1234@naver.com>
2022-12-05 11:21:52 +0900

    feat: 클래스형 컴포넌트

--- REACT/day2/src/MyComponent.jsx
+++ REACT/day2/src/MyComponent.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+/*
 const MyComponent = ({ name, children, year }) => {
   return (
     <>
@@ -21,4 +22,35 @@ MyComponent.propTypes = {
 MyComponent.defaultProps = {
   name: "미기입 시 기본값",
 };
+export default MyComponent;
+*/
+
+// 클래스형 컴포넌트
+import { Component } from "react";
+
+class MyComponent extends Component {
+  render() {
+    // props 가져오기
+    // props 속성이 존재하기 때문에 분해할당
+    const { name, year, children } = this.props;
+    return (
+      <>
+        <div>
+          <h2>이름은 {name}</h2>
+          <h2>태어난 해는 {year}</h2>
+          <h2>children 값은 {children}</h2>
+        </div>
+      </>
+    );
+  }
+}
+
+MyComponent.defaultProps = {
+  name: "기본이름",
+};
+MyComponent.propTypes = {
+  name: PropTypes.string,
+  year: PropTypes.number.isRequired,
+};
+
 export default MyComponent;
