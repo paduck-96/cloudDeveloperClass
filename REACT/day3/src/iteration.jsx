62ead35950749a5c966592076e4db5965ca3be07
paduck-96 <peterrabitt1234@naver.com>
2022-12-06 09:48:29 +0900

    feat: iteration key 값 추가

--- REACT/day3/src/iteration.jsx
+++ REACT/day3/src/iteration.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Iteration = () => {
   const names = ["Javascript", "Java", "Python", "C#", "Go", "Rust"];
-  const namesList = names.map((name) => <li>{name}</li>);
+  const namesList = names.map((name, index) => <li key={index}>{name}</li>);
   return (
     <div>
       <ul>{namesList}</ul>
