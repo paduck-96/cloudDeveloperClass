6cd51055a82b3d56cc188319bbf27caf6d1355ce
paduck-96 <peterrabitt1234@naver.com>
2022-12-06 15:34:40 +0900

    feat: userlist 작성 데이터는 App.js 파일에

--- REACT/day3/src/App.js
+++ REACT/day3/src/App.js
@@ -6,8 +6,17 @@ import ClassState from "./ClassState";
 import InputSample from "./InputSample";
 import ClassEffect from "./ClassEffect";
 import ClassEffectFunc from "./ClassEffectFunc";
+import UserList from "./UserList";
 
 function App() {
+  //UserList 받기
+  const users = [
+    { id: 1, username: "adam", email: "1234@naver.com" },
+    { id: 2, username: "jacob", email: "1421@naver.com" },
+    { id: 3, username: "pax", email: "4678@naver.com" },
+    { id: 4, username: "john", email: "198765@naver.com" },
+  ];
+
   return (
     <div>
       <ErrorBoundary>
@@ -17,6 +26,7 @@ function App() {
         <InputSample />
         <ClassEffect />
         <ClassEffectFunc />
+        <UserList users={users} />
       </ErrorBoundary>
     </div>
   );
