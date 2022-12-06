b43e8b09b9ec008385aad8814a0ad3afee2f2e19
paduck-96 <peterrabitt1234@naver.com>
2022-12-06 10:42:45 +0900

    feat: 삭제 구현 배열.slice() 사용 / 해당 항목 제외한 배열만 추가

--- REACT/day3/src/iteration.jsx
+++ REACT/day3/src/iteration.jsx
@@ -3,14 +3,48 @@ import React, { Component } from "react";
 class Iteration extends Component {
   state = {
     names: ["JavaScript"],
+    name: "",
   };
 
+  // input에 입력하면 name state의 값을 변경하는 이벤트
+  handleChange = (e) => {
+    this.setState({
+      name: e.target.value,
+    });
+  };
+  // 변경된 name을 names에 추가 이벤트
+  handleInsert = (e) => {
+    this.setState({
+      // 원본을 수정하지 않는 방식
+      // .push( )는 원본 수정
+      names: this.state.names.concat(this.state.name),
+      name: "",
+    });
+  };
+  // 데이터 삭제 함수
+  // index를 매개변수로 받아서 삭제
+  handleRemove = (index) => {
+    const { names } = this.state;
+    //slice(매개변수 2개) 받아서 배열을 잘라내는 과정
+    //시작위치와 마지막 위치 대입
+    this.setState({
+      names: [names.slice(0, index), names.slice(index + 1, names.length)],
+    });
+  };
   render() {
     const namesList = this.state.names.map((name, index) => (
-      <li key={index}>{name}</li>
+      <li key={index} onDoubleClick={(e) => this.handleRemove(index)}>
+        {name}
+      </li>
     ));
     return (
       <div>
+        <input
+          type="text"
+          onChange={this.handleChange}
+          value={this.state.name}
+        />
+        <button onClick={this.handleInsert}>추가</button>
         <ul>{namesList}</ul>
       </div>
     );
