0d5f53c7d283ba0a7fe704b48be2a3bedee75e48
paduck-96 <peterrabitt1234@naver.com>
2022-12-07 11:38:24 +0900

    feat: CSS 멀티 사용 패키지 classnames 패키지로 여러 클래스를 동시 적용 조건절 포함 가능

--- REACT/day4/src/CSSModule.jsx
+++ REACT/day4/src/CSSModule.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import classNames from "classnames/bind";
 
 import styles from "./CSSModule.module.css";
+
+// cx 안에서는 styles 생략 가능
+const cx = classNames.bind(styles);
+
 const CSSModule = () => {
   return (
-    <div className={styles.wrapper}>
+    <div className={cx("wrapper", "inverted")}>
       <span className="something">CSS 모듈</span>
     </div>
   );
