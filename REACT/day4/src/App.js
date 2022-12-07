9033994f310576d2906a1842fd097f8911d9cdf0
paduck-96 <peterrabitt1234@naver.com>
2022-12-07 12:32:33 +0900

    feat: scss 활용한 css classnames 활용해 조건절 추가

--- REACT/day4/src/App.js
+++ REACT/day4/src/App.js
@@ -1,10 +1,19 @@
 import "./App.css";
 import Average from "./Average";
 import CSSModule from "./CSSModule";
+import styles from "./App.scss";
+import classNames from "classnames/bind";
+
+const cx = classNames.bind(styles);
 
 function App() {
+  const isBlue = true;
   return (
     <div className="App">
+      <div className={cx("box", { blue: isBlue })}>
+        <div className={cx("box-inside")}></div>
+      </div>
+      <div className={cx("box-inside")}></div>
       <Average />
       <CSSModule />
     </div>
--- REACT/day4/src/CSSModule.jsx
+++ REACT/day4/src/CSSModule.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import classNames from "classnames/bind";
 
-import styles from "./CSSModule.module.css";
+import styles from "./CSSModule.module.scss";
 
 // cx 안에서는 styles 생략 가능
 const cx = classNames.bind(styles);
