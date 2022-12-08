5b1324f169a633c0dceeb04f0e2e859119127557
paduck-96 <peterrabitt1234@naver.com>
2022-12-08 10:06:46 +0900

    feat: React ToDo Main page

---
+++ REACT/todo/src/App.js
@@ -0,0 +1,12 @@
+import ToDoTemplate from "./components/ToDoTemplate";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <ToDoTemplate>To Do Application</ToDoTemplate>
+    </div>
+  );
+}
+
+export default App;
---
+++ REACT/todo/src/components/ToDoTemplate.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./ToDoTemplate.scss";
+
+//함수형 컴포넌트에서 매개변수 (children)는 상위 컴포넌트에서
+// 하위 컴포넌트를 만들 때 태그에서 넘겨준 속성들이다.
+const ToDoTemplate = ({ children }) => {
+  return (
+    <div className="ToDoTemplate">
+      <div className="app-title">일정관리</div>
+      <div className="content">{children}</div>
+    </div>
+  );
+};
+
+export default ToDoTemplate;
