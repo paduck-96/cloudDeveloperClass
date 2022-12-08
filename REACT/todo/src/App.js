3ed4d977e584a7e63761fab2fb45c3a801946ea1
paduck-96 <peterrabitt1234@naver.com>
2022-12-08 12:30:45 +0900

    feat: todoinsert app.js로부터 이벤트와 데이터 처리 가능

--- REACT/todo/src/App.js
+++ REACT/todo/src/App.js
@@ -1,10 +1,53 @@
 import ToDoTemplate from "./components/ToDoTemplate";
 import "./App.css";
+import ToDoInsert from "./components/ToDoInsert";
+import ToDoList from "./components/ToDoList";
+// useRef 는 변수 생성 OR 변수 만들어 DOM에 할당할 때
+// useCallback 은 함수를 효율적으로 생성
+import { useState, useRef, useCallback } from "react";
 
 function App() {
+  const [todos, setToDos] = useState([
+    {
+      id: 1,
+      text: "HTML, CSS, JavaScript",
+      checked: true,
+    },
+    {
+      id: 2,
+      text: "Node",
+      checked: false,
+    },
+    {
+      id: 3,
+      text: "React",
+      checked: false,
+    },
+  ]);
+
+  //아이디 변수 생성
+  const nextId = useRef(4);
+  // 삽입 처리 함수
+  // todos 에 변화 생길 경우에만 함수 재생성
+  const onInsert = useCallback(
+    (text) => {
+      const todo = {
+        id: nextId.current,
+        text,
+        checked: false,
+      };
+      setToDos(todos.concat(todo));
+      nextId.current += 1;
+    },
+    [todos]
+  );
+
   return (
     <div className="App">
-      <ToDoTemplate>To Do Application</ToDoTemplate>
+      <ToDoTemplate>
+        <ToDoInsert onInsert={onInsert} />
+        <ToDoList todos={todos} />
+      </ToDoTemplate>
     </div>
   );
 }
--- REACT/todo/src/components/ToDoInsert.jsx
+++ REACT/todo/src/components/ToDoInsert.jsx
@@ -2,11 +2,38 @@ import React from "react";
 import "./ToDoInsert.scss";
 // react-icons 의 Material Design 의 MdAdd 아이콘
 import { MdAdd } from "react-icons/md";
+import { useState, useCallback } from "react";
 
-const ToDoInsert = () => {
+const ToDoInsert = ({ onInsert }) => {
+  // 입력된 데이터 state
+  const [value, setValue] = useState("");
+
+  // 입력 내용 변경 시 호출 함수
+  //입력 시 todo 재생성 불필요
+  //삽입시 처리
+  const onChange = useCallback((e) => {
+    setValue(e.target.value);
+  }, []);
+
+  // form>submit 이벤트 발생 시 호출
+  //form>submit 시 submit 이벤트와 엔터 이벤트 동시 발생
+  //어떤 action을 받느냐에 따라 submit 혹은 button으로 할 지 결정
+  const onSubmit = useCallback(
+    (e) => {
+      onInsert(value);
+      setValue("");
+      e.preventDefault();
+    },
+    [onInsert, value]
+  );
   return (
-    <form className="ToDoInsert">
-      <input type="text" placeholder="Todo 작성" />
+    <form className="ToDoInsert" onSubmit={onSubmit}>
+      <input
+        type="text"
+        placeholder="Todo 작성"
+        value={value}
+        onChange={onChange}
+      />
       <button type="submit">
         <MdAdd />
       </button>
