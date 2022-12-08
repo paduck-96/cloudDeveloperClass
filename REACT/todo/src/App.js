b5033e3bd49b6dd7c56b28a059ce670322922015
paduck-96 <peterrabitt1234@naver.com>
2022-12-08 15:53:39 +0900

    feat: 성능최적화; useReducer 활용

--- REACT/todo/src/App.js
+++ REACT/todo/src/App.js
@@ -4,7 +4,7 @@ import ToDoInsert from "./components/ToDoInsert";
 import ToDoList from "./components/ToDoList";
 // useRef 는 변수 생성 OR 변수 만들어 DOM에 할당할 때
 // useCallback 은 함수를 효율적으로 생성
-import { useState, useRef, useCallback } from "react";
+import { useRef, useCallback, useReducer } from "react";
 
 // 대용량 데이터 생성
 const createBulkTodos = () => {
@@ -19,10 +19,34 @@ const createBulkTodos = () => {
   return array;
 };
 
+// state를 조작할 reducer 함수 생성
+const todoReducer = (todos, action) => {
+  //분기
+  switch (action.type) {
+    case "INSERT":
+      return todos.concat(action.todo);
+    case "REMOVE":
+      return todos.filter((todo) => todo.id !== action.id);
+    case "TOGGLE":
+      return todos.map((todo) =>
+        todo.id === action.id ? { ...todo, checked: !todo.checked } : todo
+      );
+    default:
+      return todos;
+  }
+};
 function App() {
   // 함수 이름을 대입해야 함수를 전부 수행하고 1번만 리랜더링
   // 호출 구문 대입 시 데이터 만들어질 때마다 리랜더링
-  const [todos, setToDos] = useState(createBulkTodos);
+  //const [todos, setToDos] = useState(createBulkTodos);
+  // reducer 설정
+  //(호출될 함수, 초기값, 호출할 메서드 - 리턴 값이 초기값)
+  // 리턴 결과는 state 의 이름 과 state 를 수정할 함수명
+  const [todos, dispatch] = useReducer(todoReducer, undefined, createBulkTodos);
+  /**
+   * 데이터 만들어주는 함수 없다면
+   * (''', 초기값, undefined)
+   */
 
   //아이디 변수 생성
   const nextId = useRef(2001);
@@ -34,22 +58,27 @@ function App() {
       text,
       checked: false,
     };
-    setToDos((todos) => todos.concat(todo));
+    //setToDos((todos) => todos.concat(todo));
+
+    dispatch({ type: "INSERT", todo });
     nextId.current += 1;
   }, []);
 
   // 데이터 삭제
   const onRemove = useCallback((id) => {
-    setToDos((todos) => todos.filter((todo) => todo.id !== id));
+    //setToDos((todos) => todos.filter((todo) => todo.id !== id));
+    dispatch({ type: "REMOVE", id });
   }, []);
 
   // 데이터 수정
   const onToggle = useCallback((id) => {
-    setToDos((todos) =>
-      todos.map((todo) =>
-        todo.id === id ? { ...todo, checked: !todo.checked } : todo
-      )
-    );
+    // setToDos((todos) =>
+    //   todos.map((todo) =>
+    //     todo.id === id ? { ...todo, checked: !todo.checked } : todo
+    //   )
+    // );
+
+    dispatch({ type: "TOGGLE", id });
   }, []);
   return (
     <div className="App">
