6f40c0b6fcd25636f79041e1f9a4777fc4e8afb5
paduck-96 <peterrabitt1234@naver.com>
2022-12-08 11:46:59 +0900

    feat: todolistiem

---
+++ REACT/todo/src/components/ToDoListItem.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./ToDoListItem.scss";
+
+import {
+  MdCheckBoxOutlineBlank,
+  MdCheckBox,
+  MdRemoveCircleOutline,
+} from "react-icons/md";
+
+import cn from "classnames";
+
+const ToDoListItem = ({ todo }) => {
+  // 넘어온 데이터 중에서 text 와 checked 만 분해
+  const { text, checked } = todo;
+  return (
+    <div className="ToDoListItem">
+      <div className={cn("checkbox", { checked })}>
+        {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
+        <div className="text">todo</div>
+      </div>
+      <div className="remove">
+        <MdRemoveCircleOutline />
+      </div>
+    </div>
+  );
+};
+
+export default ToDoListItem;
