2ce7ce08e559edaf4f4405832aed2df5098966d1
paduck-96 <peterrabitt1234@naver.com>
2022-12-09 12:33:40 +0900

    feat: query string 라이브러리

--- REACT/day6/src/About.jsx
+++ REACT/day6/src/About.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
+import qs from "qs";
 
 const About = () => {
+  // query string 읽어오기
+  const location = useLocation();
+  const queryString = qs.parse(location.search, {
+    ignoreQueryPrefix: true,
+  });
+  console.log(queryString);
   return (
     <div>
       <h1>React Router 실습</h1>
+      <p>Query String : {location.search}</p>
     </div>
   );
 };
