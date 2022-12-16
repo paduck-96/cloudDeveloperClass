d6daba83369674398d08f221303fb9876e740fb6
paduck-96 <peterrabitt1234@naver.com>
2022-12-16 17:08:19 +0900

    feat: java method and class

---
+++ JAVA/InstanceOf/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class Main {
+    static class Data implements Comparable{
+
+    }
+    public static void main(String[] args) {
+
+        int [ ] ar = {100, 300, 200, 150};
+        Arrays.sort(ar);
+        System.out.println(Arrays.toString(ar));
+
+
+    }
+}
---
+++ JAVA/MethodClass/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("static method");
+
+        //instance method
+        public void method2(){
+            System.out.println("instance method");
+        }
+    }
+}
