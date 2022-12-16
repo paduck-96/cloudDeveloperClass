3ebbde9a448440b691098b688e38e888c5c9c5ec
paduck-96 <peterrabitt1234@naver.com>
2022-12-16 17:05:02 +0900

    feat: java data and type

---
+++ JAVA/untitled/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!"+100);
+
+        Integer i = 120;
+        int [] ar = {10,20,30};
+        System.out.println(i);
+        System.out.println(i.toString());
+        System.out.println(ar);
+    }
+}
