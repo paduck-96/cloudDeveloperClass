102a78a563da5619ccde7820296127c0e7a3007c
paduck-96 <peterrabitt1234@naver.com>
2022-12-27 16:53:26 +0900

    자바 JSON 변환 및 생성

---
+++ JAVA/ExternalLibraryUse/src/main/java/main/java/JSONCreate.java
@@ -0,0 +1,26 @@
+package main.java;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.awt.*;
+import java.io.File;
+import java.util.Arrays;
+
+public class JSONCreate {
+    public static void main(String[] args){
+        ToDoVO vo1 = new ToDoVO(1,1,"한글",true);
+        ToDoVO vo2 = new ToDoVO(2,11,"영어",false);
+
+        // 저장할 JSON 파일 생성
+        File file = new File("./todo.json");
+        // JSON 기록을 위한 인스턴스 생성
+        ObjectMapper mapper = new ObjectMapper();
+        try{
+            //기록
+            mapper.writeValue(file, Arrays.asList(vo1, vo2));
+            System.out.println("기록 성공");
+        }catch (Exception e){
+            System.out.println("기록 실패");
+        }
+    }
+}
