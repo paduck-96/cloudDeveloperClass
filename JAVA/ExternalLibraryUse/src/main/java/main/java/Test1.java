3375a1d4077bb3a8ddb9610dd3f330bba30ad43c
paduck-96 <peterrabitt1234@naver.com>
2022-12-27 12:23:48 +0900

    feat: 자바 JUnit 테스트 TestCase 상속

---
+++ JAVA/ExternalLibraryUse/src/main/java/main/java/Test1.java
@@ -0,0 +1,10 @@
+package main.java;
+
+import junit.framework.TestCase;
+
+public class Test1 extends TestCase {
+public void testmethod(){
+    System.out.println(new main.java.Source().add(100,200));
+}
+}
+
