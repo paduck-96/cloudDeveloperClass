5d6c8b9dc2464186d97c241774d6adedbc94d6c8
paduck-96 <peterrabitt1234@naver.com>
2022-12-27 12:21:45 +0900

    feat: 자바 외부 라이브러리 활용 mysql 드라이버

---
+++ JAVA/ExternalLibraryUse/src/main/java/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // mysql 드라이버 로딩
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            System.out.println("드라이버 로드 성공");
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
