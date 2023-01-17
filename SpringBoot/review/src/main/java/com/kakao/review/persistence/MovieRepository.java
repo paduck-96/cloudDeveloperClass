0bc2ed6bce4dcf8e87e4b173259155eff3bd9703
paduck-96 <peterrabitt1234@naver.com>
2023-01-17 11:05:32 +0900

    feat: 영화 정보를 가지고 영화 이미지 리뷰 등 출력 테이블 left outer join 필요

--- SpringBoot/review/src/main/java/com/kakao/review/persistence/MovieRepository.java
+++ SpringBoot/review/src/main/java/com/kakao/review/persistence/MovieRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 
 import java.util.List;
@@ -12,4 +13,9 @@ import java.util.List;
 public interface MovieRepository extends JpaRepository<Movie, Long> { //엔티티 이름 과 기본키 자료형
     @Query("select m, mi, avg(coalesce(r.grade,0)), count(r.reviewnum) from Movie m left outer join MovieImage mi on mi.movie = m left outer join Review r on r.movie = m group by m")
     public Page<Object []> getList(Pageable pageable);
+
+    // 특정 영화 정보를 가지고
+    //영화 이미지 정보, 리뷰 개수 및 grade 평균
+    @Query("select m, mi, avg(coalesce(r.grade, 0)), count(r)  from Movie m left outer join MovieImage mi on mi.movie=m left outer join Review r on r.movie=m where m.mno = :mno group by mi")
+    List<Object []> getMovieWithAll(@Param("mno") Long mno);
 }
